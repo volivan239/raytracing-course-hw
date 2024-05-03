3771d5119dd858e60065e81e7d74b6091522ecdb
Ivan Volkov <volkov.ivan2004@gmail.com>
2024-05-04 01:19:59 +0300

    Another hack

--- hw8/src/include/material.h
+++ hw8/src/include/material.h
@@ -41,9 +41,9 @@ public:
 
     Vec3 brdf(const Vec3 &l, const Vec3 &v, const Vec3 &n, const Color& color, float metallic, float alpha) const {
         Vec3 h = (l + v).normalize();
-        // if (v.dot(n) < 0 || l.dot(n) < 0) {
-        //     return {0, 0, 0};
-        // }
+        if (v.dot(n) < 0 || l.dot(n) < 0) {
+            return {0, 0, 0};
+        }
         float specular = specularBrdf(l, v, n, alpha * alpha);
 
         Vec3 metalBrdf, dieletricBrdf;
@@ -54,6 +54,9 @@ public:
         }
         if (metallic < 1) {
             Vec3 diffuse = diffuseBrdf(baseColor * color);
+            // if (v.dot(n) < 0 || l.dot(n) < 0) {
+            //     diffuse = {0, 0, 0};
+            // }
             Vec3 ft = fresnelTerm({0.04, 0.04, 0.04}, {1, 1, 1}, v, h);
             dieletricBrdf = diffuse * (Vec3{1, 1, 1} - ft) + specular * ft;
         }
